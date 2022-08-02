8eea96eb3e02714cc540281aa986dc698b95efa8
peguin818 <pcc1327@gmail.com>
2022-08-02 09:26:37 +0700

    First commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Redis;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $data = Product::get();
+        //return $data;
+        return view('list', compact('data'));
+
+    }
+
+    public function add() {
+        return view('add');
+    }
+
+    public function save(Request $request) {
+        $request->validate([
+            'id' => 'required',
+            'name' => 'required',
+            'price' => 'required',
+            'detail' => 'required',
+            //'image1' => 'required',
+            'producer' => 'required',
+        ]);
+
+        $id = $request->id;
+        $name = $request->name;
+        $price = $request->price;
+        $detail = $request->detail;
+        $image1 = $request->image1;
+        $image2 = $request->image2;
+        $image3 = $request->image3;
+        $producer = $request->producer;
+
+        $product = new Product();
+        
+        $product->prdID = $id;
+        $product->prdName = $name;
+        $product->prdPrice = $price;
+        $product->prdDetail = $detail;
+        $product->prdImage1 = $image1;
+        $product->prdImage2 = $image2;
+        $product->prdImage3 = $image3;
+        $product->prdcerID = $producer;
+        $product->save();
+
+        return redirect()->back()->with('success', 'Product added successfully');
+    }
+
+    public function edit($id) {
+        $data = Product::where('prdID', '=', $id)->first();
+        return view('edit', compact('data'));
+    }
+
+    public function update(Request $request) {
+        
+        $id = $request->id;
+        Product::where('prdID', '=', $id)->update([
+            'prdName' => $request->name,
+            'prdPrice' => $request->price,
+            'prdDetail' => $request->detail,
+            'prdImage1' => $request->image1,
+            'prdImage2' => $request->image2,
+            'prdImage3' => $request->image3,
+            'prdcerID' => $request->producer
+        ]);
+
+        return redirect()->back()->with('success', 'Product update successfully');
+    }
+
+    public function delete($id) {
+        Product::where('prdID', '=', $id)->delete();
+        return redirect()->back()->with('success', 'Product delete successfully');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('index', [ProductController::class, 'index']);
+Route::get('add', [ProductController::class, 'add']);
+Route::post('save', [ProductController::class, 'save']);
+Route::get('edit/{id}', [ProductController::class, 'edit']);
+Route::post('update', [ProductController::class, 'update']);
+Route::get('delete/{id}', [ProductController::class, 'delete']);
